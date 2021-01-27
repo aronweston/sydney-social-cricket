67ffe046f224efe809d01f515ec31ccd1d71d8e3
aronweston <aron@firststepdigital.com.au>
2021-01-27 22:40:09 +1100

    heroku changes finalised

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,8 @@
 class Player < ApplicationRecord
     belongs_to :team, :optional => true
-    validates :email, :uniqueness => true, :presence => true 
-    # validates :suburb, :length: { is: 4}
     has_secure_password
+
+    validates :email, :uniqueness => true, :presence => true
+    validates :password, length: { in: 7..20}
+    # validates :suburb, :length: { is: 4}
 end
