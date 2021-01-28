9d4d8d0a354dc5f2d2cdee482cc4e87f98e42d95
aronweston <aron@firststepdigital.com.au>
2021-01-29 00:30:52 +1100

    all login and sign up routes completed, applicaiton html and nav is completed. Testing out fullcalender.io to see if it's worth passing the game data

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,24 @@
 Rails.application.routes.draw do
   root :to => "pages#home"
-  get '/landing' => "pages#landing", :as => "landing"
-  get '/users' => "pages#users", :as => "users"
+  get "/landing" => "pages#landing", :as => "landing"
+  get "/users" => "pages#users", :as => "users"
 
   resources :players, :except => [:destroy]
-  get '/players/:id/add_profile' => "players#add_profile", :as => "player_add_profile"
-  patch '/players/:id/add_profile' => "players#profile_update", :as => "player_update_profile"
-  post '/players/:id/add_profile' => "players#profile_update"
-  
-  resources :teams, :except => [:destroy] 
-  get '/teams/:id/add_profile' => "teams#add_profile", :as => "team_add_profile"
-  patch '/teams/:id/add_profile' => "teams#profile_update", :as => "team_update_profile"
-  get '/teams/join/:id' => "teams#join", :as => "join_team"
-  get '/teams/leave/:id' => "teams#leave", :as => "leave_team"
-  
+  get "/players/:id/add_profile" => "players#add_profile", :as => "player_add_profile"
+  patch "/players/:id/add_profile" => "players#profile_update", :as => "player_update_profile"
+  post "/players/:id/add_profile" => "players#profile_update"
+
+  resources :teams, :except => [:destroy]
+  get "/teams/:id/add_profile" => "teams#add_profile", :as => "team_add_profile"
+  patch "/teams/:id/add_profile" => "teams#profile_update", :as => "team_update_profile"
+  get "/teams/join/:id" => "teams#join", :as => "join_team"
+  get "/teams/leave/:id" => "teams#leave", :as => "leave_team"
+
   resources :grounds
   resources :games, :except => [:destroy]
 
-
-  get '/login' => 'session#new'
-  post '/login' => 'session#create'
-  delete '/login' => 'session#destroy'
-  get '/auth/google_oauth2/callback' => 'session#omniauth'
-
-
+  get "/login" => "session#new"
+  post "/login" => "session#create"
+  delete "/login" => "session#destroy"
+  get "/auth/google_oauth2/callback" => "session#omniauth"
 end
